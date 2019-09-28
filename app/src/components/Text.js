ef662a6445146e7806507aa1ee10a73a0dfb3e88
Brais Piñeiro <brapifra@gmail.com>
2019-09-28 15:39:49 +0100

    Use GothamRounded

--- app/src/components/Text.js
+++ app/src/components/Text.js
@@ -4,7 +4,7 @@ import { Heading } from "@kiwicom/orbit-components";
 export default function Text({ children, size = "1", style }) {
   return (
     <Heading type={`title${size}`} element={`h${size}`}>
-      <b style={style}>{children}</b>
+      <b style={{ ...style, fontFamily: "GothamRounded" }}>{children}</b>
     </Heading>
   );
 }
