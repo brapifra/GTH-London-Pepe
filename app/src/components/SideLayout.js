31264846a90f333dd7cc0bab2accfe5d8be36cae
Brais Piñeiro <brapifra@gmail.com>
2019-09-28 11:05:48 +0100

    Add react-reveal package for animations

--- app/src/components/SideLayout.js
+++ app/src/components/SideLayout.js
@@ -1,18 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const Layout = styled.div`
   display: grid;
   grid-template-columns: 2fr 3fr;
   grid-column-gap: 3rem;
   height: 100%;
+
+  & > * {
+    overflow: hidden;
+    min-width: 0;
+    height: 100%;
+  }
 `;
 
 export default function SideLayout({ aside, main }) {
   return (
     <Layout>
-      {aside}
-      {main}
+      <Fade left>
+        <aside>{aside}</aside>
+      </Fade>
+      <Fade right>
+        <main>{main}</main>
+      </Fade>
     </Layout>
   );
 }
--- app/src/views/App.js
+++ app/src/views/App.js
@@ -8,6 +8,7 @@ const Container = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+  overflow: hidden;
 `;
 
 const MainSection = styled.div`
