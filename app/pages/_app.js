226a485292f4bdb3bfd4f247651c00fd007a3e5c
Brais Piñeiro <brapifra@gmail.com>
2019-09-28 12:58:54 +0100

    Add splash screen

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
     width: 100vw;
     height: 100vh;
     margin: 0 auto;
-    background-color: ${({ theme }) => theme.orbit.paletteCloudLight};
+    background-color: '#f7f8fa';
   }
 `;
 
--- app/src/views/App.js
+++ app/src/views/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { isEmpty } from "lodash";
 import { Heading } from "@kiwicom/orbit-components";
 import styled from "styled-components";
 import Video from "src/components/Video";
 import SideLayout from "src/components/SideLayout";
 import Svg from "src/components/Svg";
 
+import { useSignalR } from "../context/SignalR";
+
 const Container = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const MainSection = styled.div`
@@ -17,19 +23,25 @@ const MainSection = styled.div`
 `;
 
 export default function App() {
+  const data = useSignalR();
+
   return (
     <Container>
-      <SideLayout
-        aside={<Video src="static/prague.mp4" startAt={37} autoPlay muted />}
-        main={
-          <MainSection>
-            <Svg height="100px" src="static/assets/Kee-logo-nav.svg" />
-            <Heading type="displaySubtitle" element="h1">
-              Prague
-            </Heading>
-          </MainSection>
-        }
-      />
+      {isEmpty(data) ? (
+        <Svg src="static/assets/Kee-logo-nav.svg" width="40%" />
+      ) : (
+        <SideLayout
+          aside={<Video src="static/prague.mp4" startAt={37} autoPlay muted />}
+          main={
+            <MainSection>
+              <Svg height="100px" src="static/assets/Kee-logo-nav.svg" />
+              <Heading type="displaySubtitle" element="h1">
+                Prague
+              </Heading>
+            </MainSection>
+          }
+        />
+      )}
     </Container>
   );
 }
